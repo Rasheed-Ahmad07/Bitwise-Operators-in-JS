a7ff2eb50ef5374f18de995224de2b816a8f59d1
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-01-16 16:57:00 +0500

    Add files via upload

---
+++ 007 bitwise operater/index.js
@@ -0,0 +1,8 @@
+// console.log("bilal");
+//^ Bitwise operaters
+//*.........  &(and), |(or) , ~(Not) , ^ (XOR) Exclusive or ,  << left shift
+// console.log(5 & 3);
+// console.log(5 | 3);
+// console.log(~5)
+console.log(5 ^ 3);
+console.log(5 >> 1);
